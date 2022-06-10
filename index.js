16c9a1eb0919f8b5d85186f4f18d26da047137ae
BarryDong <286084845@qq.com>
2022-06-10 15:47:20 +0800

    根据 url 返回不同的内容

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+const fs = require('fs')
 const http = require('http')
 const server = http.createServer()
+const path = require('path')
+const publicDir = path.resolve(__dirname, 'public')
 
 server.on('request', (request, response) => {
-  response.end('hello')
+  const { url } = request
+  console.log(url)
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html; charset=utf-8')
+      fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8')
+      fs.readFile(path.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+      fs.readFile(path.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break
+  }
 })
 
 server.listen('8888', () => {
---
+++ public/main.js
@@ -0,0 +1,4 @@
+const h1 = document.querySelector('h1')
+h1.onclick = () => {
+  alert('我是 JS')
+}
